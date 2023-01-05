7a8964dd0530a3972f3c43a746a15e07192c57da
Yash Gupta <yashgupta13.in@gmail.com>
2023-01-05 17:01:48 +0530

    adding new files

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HomeScreen.css';
+import Navbar from './Navbar';
+
+const HomeScreen = () => {
+  return (
+    <div className='homescreen'>
+        <Navbar/>
+        
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", transitionNavbar);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+    {/* only add nav__black class when show variable is true */}
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt="netflix-logo"
+        />
+
+        <img
+          className="nav__avatar"
+          src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
+          alt="avatar"
+        />
+      </div>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
