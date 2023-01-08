9feeb62b2a94f10b411e9936176b73ee83e2aa28
Yash Gupta <yashgupta13.in@gmail.com>
2023-01-09 05:24:25 +0530

    Upgraded Firebase
    Updated Firebase according to latest documentations

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDzsIU5XFPi_-qTTwfu5RFqu1coU8g2z08",
   authDomain: "netflixclone95.firebaseapp.com",
@@ -16,7 +13,12 @@ const firebaseConfig = {
   measurementId: "G-351L4L5GYV"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export {auth};
+export default db;
+
--- src/screens/SignInScreen.jsx
+++ src/screens/SignInScreen.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import './SignInScreen.css';
+import React, { useRef } from "react";
+import { auth } from "../firebase";
+import "./SignInScreen.css";
 
 const SignInScreen = () => {
+  // a big finger pointing to a HTML element
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const register = (e) => {
+    // to prevent refreshing page
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className='signupScreen'>
+    <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
-        <h4></h4>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="signupScreen__grey">New to Netflix? </span>
+          <span className="signupScreen__link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignInScreen
+export default SignInScreen;
