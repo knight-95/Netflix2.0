50323a40f17967dc845a8b7c3f70468b196e264c
Yash Gupta <yashgupta13.in@gmail.com>
2023-01-12 00:40:58 +0530

    Profile Screen
    Added ProfileScreen.jsx file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import Login from "./screens/LoginScreen"
+import Login from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "./firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
-
   const user = useSelector(selectUser);
   // const user = null;
   // const user =[];
@@ -16,22 +16,24 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      if(userAuth){
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
         // Logged In
-        // console.log(userAuth);  
-        dispatch(login({
-          uid: userAuth.uid,
-          email: userAuth.email,
-        }))
-      }else{
+        // console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
         // Logged Out
         dispatch(logout);
       }
     });
     return unsubscribe;
-  }, [])
-  
+  }, []);
+
   return (
     <div className="App">
       <Router>
@@ -42,6 +44,7 @@ function App() {
           <Login />
         ) : (
           <Routes>
+            <Route path="/profile" element={<ProfileScreen />} />
             <Route path="/" element={<HomeScreen />} />
             <Route path="/test" element={<h1>This is yash</h1>} />
           </Routes>
