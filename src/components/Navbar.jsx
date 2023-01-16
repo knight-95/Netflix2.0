0d7f8252b781f79a26f29b96abf901d2cf76bc00
Yash Gupta <yashgupta13.in@gmail.com>
2023-01-16 10:10:21 +0530

    build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
+// import db from "../firebase";
+// import { selectUser } from "../features/userSlice";
+// import { useSelector } from "react-redux";
+
 
 const Navbar = () => {
+
+  // On clicking Netflix icon, it will only allow subscribed users to go to main page
+  
+  // add code for that 
+  
   const [show, handleShow] = useState(false);
   const navigate = useNavigate();
-
+  
   const transitionNavbar = () => {
     if (window.scrollY > 100) {
       handleShow(true);
@@ -15,19 +24,19 @@ const Navbar = () => {
   };
   useEffect(() => {
     window.addEventListener("scroll", transitionNavbar);
-
+    
     return () => {
       window.removeEventListener("scroll", transitionNavbar);
     };
   }, []);
-
+  
+ 
   return (
     <div className={`nav ${show && "nav__black"}`}>
       {/* only add nav__black class when show variable is true */}
       <div className="nav__contents">
         <img
-
-          onClick={() => navigate("/")}
+          // onClick={() => navigate("/")}
 
           className="nav__logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
