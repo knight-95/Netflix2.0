7fc51af59b9f3b9b3ab4974f53f590d9b44d1c21
Yash Gupta <yashgupta13.in@gmail.com>
2023-01-16 10:23:44 +0530

    Fixed logo and description

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,8 @@ const Navbar = () => {
           // onClick={() => navigate("/")}
 
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          // src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src={require('../images/netflix_logo.png')}
           alt="netflix-logo"
         />
 
